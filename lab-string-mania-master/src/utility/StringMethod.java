c4e6cdb11bb860afa213020ef503ab5fd92131e4
Naga Sai Chaitanya <55658505+naga-sai-chaitanya@users.noreply.github.com>
2020-07-19 12:58:38 +0530

    Add files via upload

---
+++ lab-string-mania-master/src/utility/StringMethod.java
@@ -0,0 +1,24 @@
+package utility;
+import java.util.*;
+
+// Type your code
+public class StringMethod{
+	
+	public String toLowercase(String str) {
+		return str.toLowerCase();
+	}
+	
+	public String toUppercase(String str) {
+		return str.toLowerCase();
+	}
+	
+	public String firstcharacter(String str) {
+		return str.charAt(0);
+	}
+	public String Stringlength(String str) {
+		return str.length();
+	}
+	public String Stringlength(String str) {
+		return str.trim();
+	}
+}
